049747aaa9141c18cb9c919a1108b7eb651da06d
XYHub <lucien.loua@gmail.com>
2024-01-01 23:13:48 +0000

    api fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,7 @@
-export { auth } from '@/lib/auth'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { auth } from '@/lib/auth'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await auth(req, res)
+}
