179a36b8b6b379d00e21f3fc8bec44642a212c9f
XYHub <lucien.loua@gmail.com>
2024-01-14 18:45:47 +0000

    add: articles refine user avatar

--- src/components/organisms/layouts/navbar/navbar.tsx
+++ src/components/organisms/layouts/navbar/navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <nav className='md:grid grid-cols-12 border-b flex items-center justify-between relative z-50 bg-background overflow-x-auto'>
       <Link href='/' className='font-bold md:border-r md:px-5 px-2.5 py-4 text-foreground md:col-span-3 lg:col-span-2 shrink-0 transition-colors'>
-      D3v<span className='font-light bg-gradient-to-r to-[#00bef0]  from-primary bg-clip-text text-transparent'>f01i0</span>
+      D3v<span className='font-light bg-gradient-to-r to-[#00bef0] from-primary bg-clip-text text-transparent'>f01i0</span>
       </Link>
       <div className='md:col-span-9 lg:col-span-10 flex items-center justify-between'>
         <ul className='lg:flex items-center divide-x w-max border-r hidden shrink-0'>
